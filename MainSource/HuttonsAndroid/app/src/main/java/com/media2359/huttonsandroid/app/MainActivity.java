c4e9369289286a693f8fd465a9f3b183c06ae2c3
Truc Tran <truc.trantrung@2359media.com.vn>
2014-12-10 18:23:25 +0700

    test UI

--- MainSource/HuttonsAndroid/app/src/main/java/com/media2359/huttonsandroid/app/MainActivity.java
+++ MainSource/HuttonsAndroid/app/src/main/java/com/media2359/huttonsandroid/app/MainActivity.java
@@ -1,19 +1,35 @@
 package com.media2359.huttonsandroid.app;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTabHost;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.media2359.huttonsandroid.fragment.TabMoreFragment;
+import com.media2359.huttonsandroid.fragment.TabNotificationFragment;
+import com.media2359.huttonsandroid.fragment.TabProjectFragment;
+import com.media2359.huttonsandroid.fragment.TabSearchFragment;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends FragmentActivity {
+
+    public static final String DATA_TO_FRAGMENT = "MainActivity.DATA_TO_FRAGMENT";
+
+    FragmentTabHost mFragmentTabHost;
+    FragmentTabHost.TabSpec mTabProject, mTabSearch, mTabNotification, mTabMore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+
+        initWidgets();
+        initListeners();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +51,59 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Map widgets from xml layout
+     */
+
+    public void initWidgets() {
+        mFragmentTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
+//        mFragmentTabHost.setup(this, getSupportFragmentManager(), R.id.act_main_tab_content);
+        mFragmentTabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+
+        mTabProject = mFragmentTabHost.newTabSpec("Project");
+        View vProject = inflater.inflate(R.layout.tab_project, null);
+        mTabProject.setIndicator(vProject);
+
+        mTabSearch = mFragmentTabHost.newTabSpec("Search");
+//        mTabSearch.setIndicator("Search", getResources().getDrawable(R.drawable.ic_launcher));
+        View vSearch = inflater.inflate(R.layout.tab_search, null);
+        mTabSearch.setIndicator(vSearch);
+
+        mTabNotification = mFragmentTabHost.newTabSpec("Notification");
+//        mTabNotification.setIndicator("Notification", getResources().getDrawable(R.drawable.ic_launcher));
+        View vNoti = inflater.inflate(R.layout.tab_notification, null);
+        mTabNotification.setIndicator(vNoti);
+
+        mTabMore = mFragmentTabHost.newTabSpec("More");
+//        mTabMore.setIndicator("More", getResources().getDrawable(R.drawable.ic_launcher));
+        View vMore = inflater.inflate(R.layout.tab_more, null);
+        mTabMore.setIndicator(vMore);
+
+        Bundle bProject = new Bundle();
+        bProject.putString(DATA_TO_FRAGMENT, "Project");
+        mFragmentTabHost.addTab(mTabProject, TabProjectFragment.class, bProject);
+
+        Bundle bSearch = new Bundle();
+        bSearch.putString(DATA_TO_FRAGMENT, "Search");
+        mFragmentTabHost.addTab(mTabSearch, TabSearchFragment.class, bSearch);
+
+        Bundle bNoti = new Bundle();
+        bNoti.putString(DATA_TO_FRAGMENT, "Notification");
+        mFragmentTabHost.addTab(mTabNotification, TabNotificationFragment.class, bNoti);
+
+        Bundle bMore = new Bundle();
+        bMore.putString(DATA_TO_FRAGMENT, "More");
+        mFragmentTabHost.addTab(mTabMore, TabMoreFragment.class, bMore);
+    }
+
+    /**
+     *
+     * */
+    public void initListeners() {
+
     }
 }
---
+++ MainSource/HuttonsAndroid/app/src/main/java/com/media2359/huttonsandroid/fragment/TabSearchFragment.java
@@ -0,0 +1,32 @@
+package com.media2359.huttonsandroid.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.media2359.huttonsandroid.app.MainActivity;
+import com.media2359.huttonsandroid.app.R;
+
+/**
+ * Created by tructran on 12/10/14.
+ */
+public class TabSearchFragment extends Fragment {
+
+    private TextView mTvContent;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fm_tab_project, container, false);
+        mTvContent = (TextView) v.findViewById(R.id.fm_tab_project_tv_content);
+
+        Bundle b = getArguments();
+        if (b != null) {
+            mTvContent.setText(b.getString(MainActivity.DATA_TO_FRAGMENT, "There is no thing"));
+        }
+        return v;
+    }
+}
